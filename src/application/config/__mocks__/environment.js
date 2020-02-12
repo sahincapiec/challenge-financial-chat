db645ee762e60ccc9b31dcbc2bb779aa68e35c3d
Samir Hincapié <samir.hincapie@sakel.net>
2020-02-11 21:52:19 -0500

    Remove unnecessary configuration

--- src/application/config/__mocks__/environment.js
+++ src/application/config/__mocks__/environment.js
@@ -7,14 +7,12 @@ const queueStockQuote = "stock-quote-test";
 const tokenExpiration = "1 day";
 const tokenSign = "challenge-financial-chat-test";
 const viewsPath = path.join(__dirname, "..", "..", "application", "views");
-const tempPath = path.join(__dirname, "..", "..", "adapter", "temp");
 
 module.exports = {
   amqConnectionString,
   dbUrl,
   defaultPort,
   queueStockQuote,
-  tempPath,
   tokenExpiration,
   tokenSign,
   viewsPath
